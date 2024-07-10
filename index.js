0ffdfc4b79d754484fb3f39961e8ab36be82a916
StellaNjambi02 <njambindungistella@gmail.com>
2024-07-10 12:26:57 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let fetchFunc=fetch("https://anapioficeandfire.com/api/books")
+  .then((response)=>response.json())
+  .then((book)=>renderBooks(book))
+  return fetchFunc;
 }
 
 function renderBooks(books) {
